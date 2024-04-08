78c21e289285ccc5e3d70779c0c02ef668785a84
Brandon Farr <bfarr@congressasset.com>
2024-04-08 09:46:10 -0400

    Clean up 00_dependencies and reorganize open scripts

--- R/create_project.R
+++ R/create_project.R
@@ -6,16 +6,16 @@
 
 create_project <-
   function(path, ...) {
-
+    
     # Create the project path given the name chosen by the user:
     dir.create(path, recursive = TRUE, showWarnings = FALSE)
-
+    
     # Change the working directory to the recently created folder:
     setwd(file.path(getwd(), path))
-
+    
     # Collect the list of inputs in a list to be called later:
     dots <- list(...)
-
+    
     # Check .gitignore argument
     if(dots[["createGitignore"]]) {
       git_ignores <-
@@ -27,17 +27,17 @@ create_project <-
           '.Rproj.user/',
           '.Renviron'
         )
-
+      
       writeLines(paste(git_ignores, sep = '\n'), '.gitignore')
     }
-
+    
     # Check selected folder
     if(dots[["createDefaultFolders"]]) {
       dir.create("code", recursive = TRUE, showWarnings = FALSE)
       dir.create("data", recursive = TRUE, showWarnings = FALSE)
       dir.create("output", recursive = TRUE, showWarnings = FALSE)
     }
-
+    
     if(dots[["createREADME"]]) {
       readme <- c(
         '# Project Name',
@@ -53,24 +53,24 @@ create_project <-
         '',
         ''
       )
-
+      
       writeLines(paste(readme, sep = '\n'), 'README.md')
-
+      
     }
-
+    
     if(dots[["createDependencies"]]) {
       dependencies <-
-        c(
-          'library(tidyverse)',
-          'library(lubridate)',
-          'library(readxl)',
-          'library(cowplot)',
-          'library(oqthemes)',
-          'library(here)'
-        )
-
+        'suppressMessages({
+          library(tidyverse)
+          library(lubridate)
+          library(readxl)
+          library(cowplot)
+          library(oqthemes)
+          library(here)
+        })'
+      
       writeLines(paste(dependencies, sep = '\n'), 'code/00_dependencies.R')
-
+      
       first_script <-
         c(
           'source("code/00_dependencies.R")',
